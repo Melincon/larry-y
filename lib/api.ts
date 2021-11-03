e4d7785587c6f730899e0fd7b523822b2199672d
Travis Claus <melincon@gmail.com>
2021-11-02 20:23:01 -0400

    remark set up

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,10 @@
+import fc from 'fs';
+import { join } from 'path';
+import mater from 'gray-matter';
+
+// const postsDirectory = join(process.cwd(), '_posts')
+console.log(process.cwd())
+
 export function getAllProjects(fields: string[] = []) {
     const projects = null;
 }
