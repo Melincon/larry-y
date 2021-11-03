0b84eebd0efadd0a9264b78e58c5f48ca5427d83
Travis Claus <melincon@gmail.com>
2021-11-03 18:15:23 -0400

    state for project card component

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -1,14 +1,25 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import styles from './style.module.css';
 
 export interface ProjectCardProps {
     project: any;
 }
 
-const ProjectCard: FC<ProjectCardProps> = ({ project }) => (
-    <div className={styles.ProjectCard}>
-        <img className={styles.ProjectCard__bgImage} src={project.bgImage} />
-    </div>
-)
+const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
+    const [showProjectDetails,setShowProjectDetails] = useState(false);
+    const handleClick = () => {
+        setShowProjectDetails(!showProjectDetails);
+    }
+    return (
+        <div className={styles.ProjectCard}>
+            <img onClick={handleClick} className={styles.ProjectCard__bgImage} src={project.bgImage} />
+            {showProjectDetails && (
+                <div className={styles.ProjectCard__projectDetails}>
+                    testing
+                </div>
+            )}
+        </div>
+    )
+}
 
 export default ProjectCard;
